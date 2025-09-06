11c6f19a91c29fe829bd97601d5e5154df7c80cf
anthonycruz0724 <anthonycruz0724@gmail.com>
2025-09-06 14:05:15 -0500

    Initial Commit

---
+++ BeaconDectector/BeaconDectectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconDectectorApp.swift
+//  BeaconDectector
+//
+//  Created by Anthony Cruz on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BeaconDectectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeaconDectectorTests/BeaconDectectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconDectectorTests.swift
+//  BeaconDectectorTests
+//
+//  Created by Anthony Cruz on 9/6/25.
+//
+
+import Testing
+@testable import BeaconDectector
+
+struct BeaconDectectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
